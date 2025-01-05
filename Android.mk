4d5abbe10da40e00aacae2217dfab70f22fe5a5b
Michael Bestas <mkbestas@lineageos.org>
2025-01-05 04:50:37 +0200

    libstagefrighthw: Convert to blueprint
    Change-Id: I8ed83540ab990642d0956f320582a5a12cd7c207

--- Android.mk
+++ Android.mk
@@ -4,7 +4,6 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
-include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 endif
 
